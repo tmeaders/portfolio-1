717c6a672abb9b5e92e64b5ef7ed03bab218aeb3
tmeaders <tmeaders@sbcglobal.net>
2021-07-10 20:15:45 -0700

    071021

--- final/js/sevenday-s.js
+++ final/js/sevenday-s.js
@@ -1,29 +1,31 @@
-const sevendayURL =
-  "https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&appid=466dca7cca93940a33882268fd25bd99";
+const sevendayURL ="https://api.openweathermap.org/data/2.5/onecall?lat=40.45302953104331&lon=-111.89674448805302&exclude=hourly&units=imperial&appid=466dca7cca93940a33882268fd25bd99";
 fetch(sevendayURL)
   .then((response) => response.json())
   .then((jsObject) => {
+    console.log(jsObject);
     /*
      *  I am creating a variable then filtering the forecast array looking for the time 18:00:00:00
      *  I am also using an 'includes' in my filter (Referred ta as functional using filters)
      */
-    const forecast = jsObject.list.filter((x) => x.dt_txt.includes("18:00:00"));
-
+    //const forecast = jsObject.list.filter((x) => x.dt_txt.includes("18:00:00")); ***To Delete
+    const forecast = jsObject.daily;
     const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    for (let day = 0; day < forecast.length; day++) {
-      const d = new Date(forecast[day].dt_txt);
+    for (let day = 1; day < forecast.length; day++) {
+      const d = new Date(0);
+d.setUTCSeconds(forecast[day].dt);
+      //const d = new Date(forecast[day].dt_txt);
       const imagesrc =
         "https://openweathermap.org/img/w/" +
         forecast[day].weather[0].icon +
         ".png";
       const desc = forecast[day].weather[0].description;
 
-      document.getElementById(`dayofweek${day + 1}`).textContent =
+      document.getElementById(`dayofweek${day}`).textContent =
         weekdays[d.getDay()];
-      document.getElementById(`forecast${day + 1}`).textContent = Math.round(
-        forecast[day].main.temp
+      document.getElementById(`forecast${day}`).textContent = Math.round(
+        forecast[day].temp.day
       );
-      document.getElementById(`icon${day + 1}`).setAttribute("src", imagesrc);
-      document.getElementById(`icon${day + 1}`).setAttribute("alt", desc);
+      document.getElementById(`icon${day}`).setAttribute("src", imagesrc);
+      document.getElementById(`icon${day}`).setAttribute("alt", desc);
     }
   });
